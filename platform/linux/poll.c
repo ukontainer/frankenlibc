89c676416ffcfecc22884dcae356ee1fd2fcb559
Hajime Tazaki <thehajime@gmail.com>
2016-07-31 22:51:50 +0900

    lkl: make poll(2) waitable with -1

--- platform/linux/poll.c
+++ platform/linux/poll.c
@@ -1,7 +1,36 @@
 #include <poll.h>
 #include "syscall.h"
+#include "init.h"
+#include "thread.h"
+
+/* XXX tidy up */
+extern int __platform_npoll;
+extern struct pollfd __platform_pollfd[MAXFD];
 
 int poll(struct pollfd *fds, nfds_t n, int timeout)
 {
-	return syscall(SYS_poll, fds, n, timeout);
+	int i, j, ret = 0;
+	int64_t sec;
+	long nsec;
+
+	if (timeout == 0)
+		return syscall(SYS_poll, fds, n, timeout);
+
+	sec = timeout / 1000;
+	nsec = (timeout % 1000) * (1000*1000UL);
+	clock_sleep(CLOCK_REALTIME, sec, nsec);
+
+	for (i = 0; i < __platform_npoll; i++) {
+		if (__platform_pollfd[i].revents) {
+			for (j = 0; j < (int)n; j++) {
+				if (__platform_pollfd[i].fd == fds[j].fd) {
+					fds[j].revents =
+						__platform_pollfd[i].revents;
+					ret++;
+				}
+			}
+		}
+	}
+
+	return ret;
 }
