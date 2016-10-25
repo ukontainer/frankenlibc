1ef9a19c2035b663567e86ff174665ad491aa7c8
Hajime Tazaki <thehajime@gmail.com>
2016-10-25 17:41:30 +0900

    add linux/lseek as a platform call
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- platform/include/unistd.h
+++ platform/include/unistd.h
@@ -4,12 +4,17 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
+#define SEEK_SET 0
+#define SEEK_CUR 1
+#define SEEK_END 2
+
 #define fsync(f) __platform_fsync(f)
 #define getpagesize __platform_getpagesize
 #define pread(a, b, c, d) __platform_pread(a, b, c, d)
 #define pwrite(a, b, c, d) __platform_pwrite(a, b, c, d)
 #define read(f, b, c) __platform_read(f, b, c)
 #define write(f, b, c) __platform_write(f, b, c)
+#define lseek(f, o, r) __platform_lseek(f, o, r)
 
 void _exit(int) __attribute__ ((noreturn));
 int fsync(int);
@@ -18,5 +23,6 @@ ssize_t pread(int, void *, size_t, off_t);
 ssize_t pwrite(int, const void *, size_t, off_t);
 ssize_t read(int, void *, size_t);
 ssize_t write(int, const void *, size_t);
+off_t lseek(int, off_t, int);
 
 #endif
