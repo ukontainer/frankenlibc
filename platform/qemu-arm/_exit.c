bdc61672ad644ca71c59180fcf8053c427bb6725
midchildan <midchildan+git@gmail.com>
2017-08-04 19:52:10 +0900

    Fix _exit for qemu-arm

--- platform/qemu-arm/_exit.c
+++ platform/qemu-arm/_exit.c
@@ -1,9 +1,17 @@
 #include <unistd.h>
 
-void
-_exit(int status)
+void _exit(int status)
 {
+	if (status == 0) {
+		__asm__ __volatile__("mov r0, #0x18;\n"
+				     "ldr r1, =#0x20026\n"
+				     "svc 0x00123456;\n"
+				     ::: "r0", "r1");
+	}
 
-	/* XXX does not return status */
-	__asm__ __volatile__ ("mov r0, #0x18;\nmov r1, %0\nsvc 0x00123456;\n"::"r"(status):"r0", "r1");
+	__asm__ __volatile__("mov r0, #0x18;\n"
+			     "mov r1, %0\n"
+			     "svc 0x00123456;\n"
+			     :: "r"(status)
+			     : "r0", "r1");
 }
