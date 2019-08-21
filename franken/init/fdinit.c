ukontainer/frankenlibc

commit 37ac2693ef7ef0b3ab6f6252e227e16651e07911

more quiet warning messages
Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

franken/init/fdinit.c
@@ -130,15 +130,20 @@ static void term_9psv(int sig)
 
 void __franken_fdinit()
 {
-	int n, x, fd;
+	int n, fd;
+	size_t x;
 	char *env, *var;
 	struct stat st;
 
 	/* retrieve fd from environ embedded by rexec. */
 
-
-
 	for (n = 0; n <= 2; n++) {
+		memset(&st, 0, sizeof(struct stat));
+		if (fstat(n, &st) == -1) {
+			__franken_fd[n].valid = 0;
+			continue;
+		}
+
 		/* make STDIN, STDOUT, STDERR valid */
 		__franken_fd[n].valid = 1;
 		__franken_fd[n].flags = fcntl(n, F_GETFL, 0);
@@ -161,11 +166,16 @@ void __franken_fdinit()
 		else
 			continue;
 
+		fd = atoi(var);
 		if (strncmp(env, "__RUMP_FDINFO_", 14) == 0||
 		    strncmp(env, "9PFS_FD", 7) == 0) {
-			fd = atoi(var);
 			__franken_fd[fd].valid = 1;
 			__franken_fd[fd].flags = fcntl(fd, F_GETFL, 0);
+
+			memset(&st, 0, sizeof(struct stat));
+			if (fstat(n, &st) == -1) {
+				__franken_fd[n].valid = 0;
+			}
 			memcpy(&__franken_fd[fd].st, &st, sizeof(struct stat));
 		}
 
@@ -531,7 +541,6 @@ __franken_fdinit_create()
 	if (getenv("9PFS_MNT")) {
 		char *mnt_point = getenv("9PFS_MNT");
 		int ret;
-		char buf[64];
 
 		if (strcmp(mnt_point, "/") == 0) {
 			ret = lkl_sys_mkdir("/mnt", 0700);
@@ -565,10 +574,10 @@ __franken_fdinit_create()
 	mount_tmpfs();
 	/* mount procfs */
 	rump___sysimpl_mkdir("/proc", 0777);
-	rump___sysimpl_mount50("proc", "/proc", 0, NULL, NULL);
+	rump___sysimpl_mount50("proc", "/proc", 0, NULL, 0);
 	/* mount sysfs */
 	rump___sysimpl_mkdir("/sys", 0777);
-	rump___sysimpl_mount50("sysfs", "/sys", 0, NULL, NULL);
+	rump___sysimpl_mount50("sysfs", "/sys", 0, NULL, 0);
 }
 
 void franken_recv_thread(int fd, void *thrid)

franken/init/init.h
@@ -5,6 +5,7 @@
 #include <netinet/in.h>
 
 #define MAXFD 64
+int printf(const char *fmt, ...);
 
 struct __fdtable {
 	int valid;
@@ -30,6 +31,6 @@ struct lkl_config *franken_lkl_get_json_config(void);
 void darwin_mod_init_func(void);
 void darwin_mod_term_func(void);
 #else
-static void inline darwin_mod_init_func(void) {}
-static void inline darwin_mod_term_func(void) {}
+static inline void darwin_mod_init_func(void) {}
+static inline void darwin_mod_term_func(void) {}
 #endif

franken/init/start_main.c
@@ -57,7 +57,6 @@ finifn()
 int
 __franken_start_main(int(*main)(int,char **,char **), int argc, char **argv, char **envp)
 {
-	uintptr_t a;
 
 	environ = envp;
 	_environ = envp;
@@ -90,6 +89,7 @@ __franken_start_main(int(*main)(int,char **,char **), int argc, char **argv, cha
 	rump_pub_lwproc_rfork(0);
 
 #ifndef MUSL_LIBC
+	uintptr_t a;
 	/* init NetBSD libc */
 	_libc_init();
 

tests/fork-test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <errno.h>
 
@@ -25,8 +26,9 @@ main(int argc, char *argv[])
 
 	if (pid == 0) {
 		// child
-		lkl_printf("child pid=%d, ppid=%d\n", getpid(), getppid());
-		ret = execve(path, e_argv, e_envv);
+		printf("child pid=%d, ppid=%d\n", getpid(), getppid());
+		ret = execve(path, (char *const *)e_argv,
+			     (char *const *)e_envv);
 		if (ret < 0)
 			perror("execve error");
 
@@ -34,13 +36,13 @@ main(int argc, char *argv[])
 		return -1;
 	} else {
 		// parent
-		lkl_printf("parent ch pid=%d, pa pid=%d\n", pid, 0);
+		printf("parent ch pid=%d, pa pid=%d\n", pid, 0);
 		/* XXX: since ld.so calls exit(main), this won't be called...  */
 		if ((ret = waitid(P_PID, pid, NULL, WEXITED)) < 0) {
-			lkl_perror("wait error", errno);
+			perror("wait error");
 			return -1;
 		}
-		lkl_printf("The child (pid=%d) existed with status(%d).\n",
+		printf("The child (pid=%d) existed with status(%d).\n",
 		       pid, WEXITSTATUS(status));
 
 	}

commit 81f7b037839dc1b83e1ee3300ef47ef9796828b1

franken: fix crash issue on regular startup
Fixes: 37ac2693 ("more quiet warning messages")

Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

franken/init/fdinit.c
@@ -163,7 +163,7 @@ void __franken_fdinit()
 			__franken_fd[fd].flags = fcntl(fd, F_GETFL, 0);
 
 			memset(&st, 0, sizeof(struct stat));
-			if (fstat(n, &st) == -1) {
+			if (fstat(fd, &st) == -1) {
 				__franken_fd[n].valid = 0;
 			}
 			memcpy(&__franken_fd[fd].st, &st, sizeof(struct stat));