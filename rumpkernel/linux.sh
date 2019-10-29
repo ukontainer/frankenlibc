ukontainer/frankenlibc

commit 20688d62009b1f5b3fd693b9e5da6718fd7da5e6

fix build issues on arm-linux-gnueabihf target
This uses new bintray repository capable with multiple arch for docker
image.  It also adds a new test target using arm32 cross compilation.

Various issues on 32-bit architecture are also fixed in musl and lkl
(syscall and struct stat convention).

Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

rumpkernel/linux.sh
@@ -35,11 +35,11 @@ rumpkernel_createuserlib()
 	cd musl
 	LKL_HEADER="${RUMP}/"
 	CIRCLE_TEST_REPORTS="${CIRCLE_TEST_REPORTS-./}"
-	CC=gcc ./configure --with-lkl=${LKL_HEADER} --disable-shared --enable-debug \
+	CC=${CC:-gcc} ./configure --with-lkl=${LKL_HEADER} --disable-shared --enable-debug \
 		    --disable-optimize --prefix=${RUMPOBJ}/musl CFLAGS="${EXTRA_CFLAGS}"
 	# XXX: bug of musl Makefile ?
 	${MAKE} obj/src/internal/version.h
-	CC=gcc ${MAKE} install
+	CC=${CC:-gcc} ${MAKE} install
 	# install libraries
 	${INSTALL-install} -d ${OUTDIR}/lib
 	${INSTALL-install} ${RUMPOBJ}/musl/lib/libpthread.a \
@@ -112,9 +112,17 @@ rumpkernel_explode_libc()
 	fi
 
 	cp ${RUMPOBJ}/${RUMP_KERNEL}.o ./
+
+	LKL_CROSS=$(${CC} -dumpmachine)
+	if [ ${LKL_CROSS} = "$(cc -dumpmachine)" ]
+	then
+		LKL_CROSS=
+	else
+		LKL_CROSS=${LKL_CROSS}-
+	fi
 	# XXX: ld.gold generates _end BSS symbol at link time
 	if [ ${OS} = "linux" ] ; then
-		objcopy --redefine-sym _end=rumpns__end ${RUMPOBJ}/linux/tools/lkl/liblkl.a
+		${LKL_CROSS}objcopy --redefine-sym _end=rumpns__end ${RUMPOBJ}/linux/tools/lkl/liblkl.a
 	fi
 )
 	LIBC_DIR=musl

.travis-deploy.sh
@@ -20,8 +20,8 @@ cp rumpobj/tests/lkick $DESTDIR/bin
 # deploy to bintray
 tar cfz /tmp/frankenlibc.tar.gz $DESTDIR
 curl -T /tmp/frankenlibc.tar.gz -u$BINTRAY_USER:$BINTRAY_APIKEY \
-     "https://api.bintray.com/content/libos-nuse/x86_64-rumprun-linux/frankenlibc/dev/$TRAVIS_OS_NAME/$PUB_FILENAME;override=1;publish=1"
+     "https://api.bintray.com/content/ukontainer/ukontainer/frankenlibc/dev/$TRAVIS_OS_NAME/$ARCH/$PUB_FILENAME;override=1;publish=1"
 # publish !
 curl -X POST -u$BINTRAY_USER:$BINTRAY_APIKEY \
-     https://api.bintray.com/content/libos-nuse/x86_64-rumprun-linux/frankenlibc/dev/publish
+     https://api.bintray.com/content/ukontainer/ukontainer/frankenlibc/dev/publish
 

.travis.yml
@@ -15,16 +15,21 @@ jobs:
       dist: trusty
       sudo: required
       compiler: clang
+      env:
+        - ARCH=amd64
     - stage: test
       os: linux
       dist: trusty
       sudo: required
       compiler: gcc
+      env:
+        - ARCH=amd64
     - stage: test
       os: osx
       osx_image: xcode10.1
       compiler: clang
       env: DEPLOY=skip
+        - ARCH=amd64
       before_install:
         - mkdir -p ~/.local/bin
         - mkdir ~/junit
@@ -40,6 +45,8 @@ jobs:
       os: osx
       osx_image: xcode10.2
       compiler: clang
+      env:
+        - ARCH=amd64
       before_install:
         - mkdir -p ~/.local/bin
         - mkdir ~/junit
@@ -51,6 +58,18 @@ jobs:
         - ln -sf /usr/local/bin/gdate ~/.local/bin/date
         - ln -sf /usr/local/opt/e2fsprogs/sbin/mkfs.ext4 ~/.local/bin/
         - ln -sf /usr/local/opt/binutils/bin/objcopy ~/.local/bin/objcopy
+    - stage: test
+      os: linux
+      dist: trusty
+      sudo: required
+      compiler: arm-linux-gnueabihf-gcc
+      env:
+        - CC=arm-linux-gnueabihf-gcc
+        - PATH=/usr/lib/ccache/:$HOME/rpi-tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/:$PATH
+        - ARCH=arm
+      before_install:
+        - git clone https://github.com/raspberrypi/tools $HOME/rpi-tools
+        - sudo ln -s `which ccache` /usr/lib/ccache/arm-linux-gnueabihf-gcc
     - stage: downstream
       before_script:
       script: bash -ex ./.travis-trigger.sh ukontainer/rumprun-packages lkl-musl
@@ -64,5 +83,5 @@ script:
   - for file in `find patches/ -maxdepth 1 -type f` ; do patch -p1 < $file ; done
   - for file in `find patches/$TRAVIS_OS_NAME/ -maxdepth 1 -type f` ; do patch -p1 < $file ; done
   - ./build.sh -d /opt/rump -q -k linux notest
-  - RUMPDIR=/opt/rump RUMPOBJ=`pwd`/rumpobj make -C tests run
+  - if [ $CC != "arm-linux-gnueabihf-gcc" ] ; then RUMPDIR=/opt/rump RUMPOBJ=`pwd`/rumpobj make -C tests run ; fi
   - bash -ex ./.travis-deploy.sh $DEPLOY

build.sh
@@ -535,8 +535,12 @@ mkdir -p ${RUMPOBJ}/explode/platform
 
 	cd ${RUMPOBJ}/explode
 	${AR-ar} cr libc.a rumpkernel/rumpkernel.o rumpuser/*.o ${LIBC_DIR}/*.o franken/*.o platform/*.o
-	if [ "${HOST}" = "Linux" ]; then
-		${CC-cc} -fuse-ld=gold -Wl,-e,_dlstart -nostdlib -shared -o libc.so \
+	if [ "${HOST}" = "Linux" ] && [ "${OS}" != "qemu-arm" ]; then
+		LDFLAGS_LIBCSO="-fuse-ld=gold -Wl,-e,_dlstart -nostdlib -shared"
+		if [ "${CC}" = "arm-linux-gnueabihf-gcc" ] ; then
+			appendvar LDFLAGS_LIBCSO "-static"
+		fi
+		${CC-cc} $LDFLAGS_LIBCSO -o libc.so \
 			 rumpkernel/rumpkernel.o rumpuser/*.o ${LIBC_DIR}/*.o franken/*.o \
 			 platform/*.o ${LIBC_DIR}/*.lo -lgcc -lgcc_eh
 	fi
@@ -555,7 +559,7 @@ rumpkernel_install_extra_libs
 ${INSTALL-install} ${RUMP}/lib/*.o ${OUTDIR}/lib
 [ -f ${RUMP}/lib/libg.a ] && ${INSTALL-install} ${RUMP}/lib/libg.a ${OUTDIR}/lib
 ${INSTALL-install} ${RUMPOBJ}/explode/libc.a ${OUTDIR}/lib
-if [ "${HOST}" = "Linux" ]; then
+if [ "${HOST}" = "Linux" ] && [ "${OS}" != "qemu-arm" ]; then
 	${INSTALL-install} ${RUMPOBJ}/explode/libc.so ${OUTDIR}/lib
 	LDSO_PATHNAME="${OUTDIR}/lib/ld-frankenlibc-x86_64-linux.so.1"
 	WD=`pwd`; cd ${OUTDIR}/lib/; ln -sf libc.so ${LDSO_PATHNAME}; cd ${WD}
@@ -565,7 +569,9 @@ fi
 # select these based on compiler defs
 if $(${CC-cc} -v 2>&1 | grep -q clang)
 then
-	TOOL_PREFIX=$(basename $(ls ${RUMPOBJ}/tooldir/bin/*-clang) | sed -e 's/-clang//' -e "s/--netbsd/-rumprun-${RUMP_KERNEL}/")
+	TOOL_PREFIX=$(basename $(ls ${RUMPOBJ}/tooldir/bin/*-clang) | \
+			  sed -e 's/-clang//' -e "s/--netbsdelf-/-rumprun-${RUMP_KERNEL}-/" \
+			      -e "s/--netbsd/-rumprun-${RUMP_KERNEL}/" -e "s/-eabihf//")
 	# possibly some will need to be filtered if compiler complains. Also de-dupe.
 	COMPILER_FLAGS="-fno-stack-protector -Wno-unused-command-line-argument ${EXTRA_CPPFLAGS} ${UNDEF} ${EXTRA_CFLAGS} ${EXTRA_LDSCRIPT_CC}"
 	appendvar COMPILER_FLAGS "-isystem ${OUTDIR}/include -nostdinc"
@@ -601,7 +607,8 @@ then
 else
 	# spec file for gcc
 	TOOL_PREFIX=$(basename $(ls ${RUMPOBJ}/tooldir/bin/*-gcc) | \
-			  sed -e 's/-gcc//' -e "s/--netbsd/-rumprun-${RUMP_KERNEL}/")
+			  sed -e 's/-gcc//' -e "s/--netbsdelf-/-rumprun-${RUMP_KERNEL}-/" \
+			      -e "s/--netbsd/-rumprun-${RUMP_KERNEL}/" -e "s/-eabihf//")
 	COMPILER_FLAGS="-fno-stack-protector ${EXTRA_CFLAGS}"
 	COMPILER_FLAGS="$(echo ${COMPILER_FLAGS} | sed 's/--sysroot=[^ ]*//g')"
 	[ -f ${OUTDIR}/lib/crt0.o ] && appendvar STARTFILE "${OUTDIR}/lib/crt0.o"

commit 7f9afdba6f7d3a3d53de0074dc62425d8155ad9a

lkl: fix up clang case for arm32 build
Fixes: 20688d62 ("fix build issues on arm-linux-gnueabihf target")

Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

rumpkernel/linux.sh
@@ -33,6 +33,13 @@ rumpkernel_createuserlib()
 	set -e
 	echo "=== building musl ==="
 	cd musl
+
+	# musl needs to be built with gcc for libc.so
+        if [ -n "$(${CC} -v 2>&1 | grep "clang")" ]; then
+		# XXX: should properly handle cross-build environment
+		CC=gcc
+	fi
+
 	LKL_HEADER="${RUMP}/"
 	CIRCLE_TEST_REPORTS="${CIRCLE_TEST_REPORTS-./}"
 	CC=${CC:-gcc} ./configure --with-lkl=${LKL_HEADER} --disable-shared --enable-debug \
@@ -120,6 +127,11 @@ rumpkernel_explode_libc()
 	else
 		LKL_CROSS=${LKL_CROSS}-
 	fi
+	# clang does not have triple prefix
+        if [ -n "$(${CC} -v 2>&1 | grep "clang")" ]; then
+		LKL_CROSS=
+	fi
+
 	# XXX: ld.gold generates _end BSS symbol at link time
 	if [ ${OS} = "linux" ] ; then
 		${LKL_CROSS}objcopy --redefine-sym _end=rumpns__end ${RUMPOBJ}/linux/tools/lkl/liblkl.a