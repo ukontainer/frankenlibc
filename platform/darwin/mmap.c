cadf011ae3e488f48b7fd0fdd0e07d69dd6d4334
Hajime Tazaki <thehajime@gmail.com>
2020-08-06 11:42:17 +0900

    fixup xcode12 llvm

--- platform/darwin/mmap.c
+++ platform/darwin/mmap.c
@@ -6,3 +6,8 @@ void *mmap(void *mem, size_t length, int prot, int flags, int fd, off_t offset)
 	return (void *)syscall_6(SYS_mmap, (intptr_t)mem, length,
 				 prot, flags, fd, offset);
 }
+
+/* XXX: llvm w/ Xcode12 generates this even -fno-stack-protector is. */
+void ___chkstk_darwin(void)
+{
+}
